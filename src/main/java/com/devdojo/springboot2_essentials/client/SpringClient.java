05aa3babc7d677be7cea6db9f133b4889382e3aa
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-10-07 19:23:06 -0300

    RestTemplate POST

--- src/main/java/com/devdojo/springboot2_essentials/client/SpringClient.java
+++ src/main/java/com/devdojo/springboot2_essentials/client/SpringClient.java
@@ -3,6 +3,7 @@ package com.devdojo.springboot2_essentials.client;
 import com.devdojo.springboot2_essentials.anime.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -23,10 +24,25 @@ public class SpringClient {
         Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
         log.info(Arrays.toString(animes));
 
-        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null,
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
                 new ParameterizedTypeReference<List<Anime>>() {
                 });
-
         log.info(exchange.getBody());
+
+
+        Anime naruto = Anime.builder().name("Naruto").build();
+        Anime narutoSaved = new RestTemplate().postForObject("http://localhost:8080/animes",
+                naruto,
+                Anime.class);
+        log.info("saved anime: "+ narutoSaved);
+
+        Anime attackOnTitan = Anime.builder().name("Attack On Titan").build();
+        ResponseEntity<Anime> aotSaved = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(attackOnTitan),
+                Anime.class);
+        log.info(aotSaved);
     }
 }
