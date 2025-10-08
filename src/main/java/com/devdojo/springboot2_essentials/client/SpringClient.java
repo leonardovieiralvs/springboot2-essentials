dacac3820ce81d5808cfaaad2fa11407667c3994
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-10-08 05:43:52 -0300

    RestTemplate PUT and DELETE

--- src/main/java/com/devdojo/springboot2_essentials/client/SpringClient.java
+++ src/main/java/com/devdojo/springboot2_essentials/client/SpringClient.java
@@ -15,9 +15,12 @@ import java.util.List;
 public class SpringClient {
     public static void main(String[] args) {
 
+    /*
+        //GET + DETAILS
         ResponseEntity<Anime> forEntity = new RestTemplate().getForEntity("http://localhost:8080/animes/2", Anime.class);
         log.info(forEntity);
 
+        //GET ONLY BODY
         Anime forObject = new RestTemplate().getForObject("http://localhost:8080/animes/2", Anime.class);
         log.info(forObject);
 
@@ -30,19 +33,43 @@ public class SpringClient {
                 new ParameterizedTypeReference<List<Anime>>() {
                 });
         log.info(exchange.getBody());
+    */
 
-
+        //POST ONLY BODY
         Anime naruto = Anime.builder().name("Naruto").build();
         Anime narutoSaved = new RestTemplate().postForObject("http://localhost:8080/animes",
                 naruto,
                 Anime.class);
-        log.info("saved anime: "+ narutoSaved);
+        log.info("ANIME CREATED ONLY BODY: " + narutoSaved);
 
+        //POST MORE DETAILS
         Anime attackOnTitan = Anime.builder().name("Attack On Titan").build();
-        ResponseEntity<Anime> aotSaved = new RestTemplate().exchange("http://localhost:8080/animes",
+        ResponseEntity<Anime> attackOnTitanSaved = new RestTemplate().exchange("http://localhost:8080/animes",
                 HttpMethod.POST,
                 new HttpEntity<>(attackOnTitan),
                 Anime.class);
-        log.info(aotSaved);
+        log.info("ANIME CREATED RESPONSE ENTITY: " + attackOnTitanSaved);
+
+
+        //PUT
+        Anime attackOnTitanUpdate = attackOnTitanSaved.getBody();
+        attackOnTitanUpdate.setName("AttackOnTitan 2");
+
+        ResponseEntity<Anime> attackOnTitanUpdate2 = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.PUT,
+                new HttpEntity<>(attackOnTitanUpdate),
+                Anime.class);
+
+        log.info(attackOnTitanUpdate2);
+
+
+        //DELETE
+        ResponseEntity<Void> animeDelete = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class, attackOnTitanUpdate.getId());
+
+        log.info("Delete ID: " +attackOnTitanUpdate.getId() + animeDelete);
+
     }
 }
